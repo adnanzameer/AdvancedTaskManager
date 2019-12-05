bafab4019c06a7b18a3446395519a8231d89416f
EP\adza <adnan.zameer@episerver.com>
2019-12-05 16:39:21 +0000

    update assemblyname

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 [assembly: AssemblyTitle("Advanced Task")]
 [assembly: AssemblyDescription("Advanced task for Episerver. See http://www.adnanzameer.com")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Advanced task")]
+[assembly: AssemblyProduct("Advanced Task Manager")]
 [assembly: AssemblyCopyright("Adnan Zameer")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
