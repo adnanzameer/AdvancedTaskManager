d19282f8b222e9501ca5b84e18de258ad29eb88f
EP\adza <adnan.zameer@episerver.com>
2019-12-05 18:41:59 +0000

    hide tab

---
+++ src/Business/HideContentApprovalTabDescriptor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using EPiServer.Core;
+using EPiServer.Shell.ObjectEditing;
+using EPiServer.Shell.ObjectEditing.EditorDescriptors;
+
+namespace AdvancedTask.Business
+{
+    [EditorDescriptorRegistration(TargetType = typeof(ContentData))]
+    public class HideCategoryEditorDescriptor : EditorDescriptor
+    {
+        public override void ModifyMetadata(ExtendedMetadata metadata, IEnumerable<Attribute> attributes)
+        {
+            bool enableContentApprovalDeadline = bool.Parse(ConfigurationManager.AppSettings["ATM:EnableContentApprovalDeadline"] ?? "false");
+
+            if (enableContentApprovalDeadline)
+            {
+                return;
+            }
+
+            foreach (ExtendedMetadata property in metadata.Properties)
+            {
+                if (property.GroupSettings != null && string.Equals(property.GroupSettings.Name, "Content Approval", StringComparison.OrdinalIgnoreCase))
+                {
+                    property.GroupSettings.DisplayUI = false;
+                    return;
+
+                }
+            }
+        }
+    }
+}
